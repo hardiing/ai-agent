57d7bb8d6308d8e47d5b8f81a5ce1dc2ab4ff9b1
Hardiing <nathanharding02@gmail.com>
2025-09-25 00:25:48 -0600

    Ch. 1-5

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 def main():
     print("Hello from bootdev-llm!")
-    if len(sys.argv[1]) > 3:
-        response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=sys.argv)
-    else:
-        print("error")
-        sys.exit(1)
+    #if len(sys.argv[1]) > 3:
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages)
+    #else:
+        #print("error")
+        #sys.exit(1)
     print(response.text)
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
